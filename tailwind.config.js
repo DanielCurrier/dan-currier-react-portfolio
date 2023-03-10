baca96350dfb5c402eb5a07a48a80294cfd35664
LordDanklin <dcurrier585@gmail.com>
2023-03-10 11:10:56 -0500

    changed file structure, created About component and included tailwind.css to the project!

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app.js", "./src/components/About.js", "./src/components/Contact.js", "./src/components/Navbar.js", "./src/components/Projects.js", "./src/components/Skills.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
