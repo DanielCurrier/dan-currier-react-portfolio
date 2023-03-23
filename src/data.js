72bcfebb2b0a0b706727117fbe61453f8d87420b
LordDanklin <dcurrier585@gmail.com>
2023-03-22 20:32:36 -0400

    new project added!

--- src/data.js
+++ src/data.js
@@ -27,6 +27,13 @@ export const projects = [
         image: require("./images/snake-game.gif"),
         link: "https://lorddanklin.github.io/snake-game/",
     },
+    {
+        title: "Couchy",
+        subtitle: "Final Bootcamp Project",
+        description: "A travel accomaodation website that provides listings from the unlikliest of places. Where's my couch?",
+        image: require("./images/couchy.png"),
+        link: "https://the-real-couchy.herokuapp.com/",
+    }
 ]
 
 export const skills = ["JavaScript", "Node.js", "MySQL", "React", "Express.js", "MongoDB", "Progressive Web Applications", "CSS", "HTML", "Public Speaking", "Writing", "Communications"];
