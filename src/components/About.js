6c011bd399e45b4494613950fe77e40bd9a23b8b
LordDanklin <dcurrier585@gmail.com>
2023-05-24 11:26:39 -0400

    Personal edits and privacy update

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,54 @@
-
 import React from "react";
 
 export default function About() {
-    return (
-        <section id="about">
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" >
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hey there! My name is Dan Currier.
-                        <br className="hidden lg:inline-block" /> I like full stack development.
-                    </h1>
-                    <p className="mb-8 leading-relaxed">
-                        Hi there, My name is Daniel Currier, and welcome to the rebuild of my portfolio website! As a fledgling
-                        full-stack student, my goal is to use this website as a testing ground and library for all sorts of computer science projects.
-                        <br></br>A
-                        little more about me: I am an Eagle Scout and a 2019 International Distinguished Chapter Member of Phi Theta Kappa Honors Society. I have recently graduated cum laude from Ramapo College of New Jersey with a degree
-                        in Communications-Writing. After trying my hand in the business, I decided that it was not for me, and began
-                        my journey in computer programming. My ultimate goal is to become a game designer, and be the
-                        most capable person for the job! Hobbies include PC gaming, tabletop gaming, hiking, bouldering, literature, martial arts
-                        and medieval swordsmanship.
-                    </p>
-                    <div className="flex justify-center">
-                        <a
-                            href="#contact"
-                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"> Let's Work Together!
-                        </a>
-                        <a href="#projects"
-                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Here are some of my best works.
-                        </a>
-                    </div>
-                </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img className="object-cover object-center rounded"
-                        alt="hero"
-                        src={require("../images/profile.jpg")}
-                    />
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hey there! My name is Dan Currier.
+            <br className="hidden lg:inline-block" /> I like full stack
+            development.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Hi there, My name is Daniel Currier, and welcome to my portfolio
+            website! As a fledgling full-stack developer, my goal is to use this
+            website as a testing ground and library for all sorts of computer
+            science projects.
+            <br></br>A little more about me: I am an Eagle Scout and a 2019
+            International Distinguished Chapter Member of Phi Theta Kappa Honors
+            Society. I have recently graduated cum laude from Ramapo College of
+            New Jersey with a degree in Communications-Writing. After trying my
+            hand in the business, I decided that it was not for me, and began my
+            journey in computer programming. My ultimate goal is to become a
+            game developer, and be the most capable person for the job! Hobbies
+            include PC gaming, tabletop gaming, hiking, bouldering, literature,
+            martial arts and medieval swordsmanship.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              {" "}
+              Let's Work Together!
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              Here are some of my best works.
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src={require("../images/profile.jpg")}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,100 +1,137 @@
 import React from "react";
 
-
 export default function Contact() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
-    function encode(data) {
-        return Object.keys(data)
-            .map(
-                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-            ).join("&");
-    }
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", name, email, message }),
-        })
-            .then(() => alert("Message sent!"))
-            .catch((err) => alert(err));
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((err) => alert(err));
+  }
 
-    }
-
-
-    return (
-        <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        title="map"
-                        className="absolute inset-0  border-0 my-0"
-                        style={{ filter: "opacity(0.7)" }}
-                        src="https://www.google.com/maps/embed/v1/place?q=51+Hartland+Avenue,+Emerson,+NJ,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" />
-                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                        <div className="lg:w-1/2 px-6">
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                ADDRESS
-                            </h2>
-                            <p className="mt-1">
-                                52 Hartland Ave. <br />
-                                Emerson, NJ 07630
-                            </p>
-                        </div>
-                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs"> GITHUB</h2>
-                            <a className=" text-green-400 leading-relaxed" href="https://github.com/LordDanklin">LordDanklin</a>
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                                EMAIL
-                            </h2>
-                            <p className="text-green-400 leading-relaxed">dcurrier585@gmail.com</p>
-                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">CALL ME</h2>
-                            <p className=" leading-relaxed">201-478-2196</p>
-                        </div>
-                    </div>
-                </div>
-
-                <form netlify="true" name="contact" onSubmit={handleSubmit} className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                    <h2 className="text-white sm:text-4x1 text-3xl mb-1 font-medium title-font">
-                        Contact Me!
-                    </h2>
-                    <p className="leading-relaxed mb-5">I am currently seeking internships or full-time employment. If you are interested in what I can provide for you, leave a name, email and message for me. I'd love to hear from you😁</p>
-                    <div className="relative mb-4">
-                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900  text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setName(e.target.value)} />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">Email</label>
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="message" className="leading-7 text-sm text-gray-400">Message</label>
-                        <input
-                            type="text"
-                            id="message"
-                            name="message"
-                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setMessage(e.target.value)} />
-                    </div>
-                    <button type="submit" className="text-white bg-green-500 oborder-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
-                </form>
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0  border-0 my-0"
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=Emerson,+NJ,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                <br />
+                Emerson, NJ 07630
+              </p>
             </div>
-        </section>
-    );
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                {" "}
+                GITHUB
+              </h2>
+              <a
+                className=" text-green-400 leading-relaxed"
+                href="https://github.com/DanielCurrier"
+              >
+                DanielCurrier
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <p className="text-green-400 leading-relaxed">
+                dcurrier585@gmail.com
+              </p>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                CALL ME
+              </h2>
+              <p className=" leading-relaxed">201-478-2196</p>
+            </div>
+          </div>
+        </div>
+
+        <form
+          netlify="true"
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
+          <h2 className="text-white sm:text-4x1 text-3xl mb-1 font-medium title-font">
+            Contact Me!
+          </h2>
+          <p className="leading-relaxed mb-5">
+            I am currently seeking internships or full-time employment. If you
+            are interested in what I can provide for you, leave a name, email
+            and message for me. I'd love to hear from you😁
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900  text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400"
+            >
+              Message
+            </label>
+            <input
+              type="text"
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-green-500 oborder-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
