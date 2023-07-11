a52d6c9886f843b66d99be1d95fed78832aa2ed4
LordDanklin <dcurrier585@gmail.com>
2023-07-11 12:01:43 -0400

    removed class hw

--- src/data.js
+++ src/data.js
@@ -1,39 +1,41 @@
 export const projects = [
-    {
-        title: "Phantom Plates",
-        subtitle: "Model View Controllers",
-        description: "An ecommerce application designed to provide foodies with delicious homemade meals and homecooks with an outlet for their passion.",
-        image: require("./images/phantom-plates.png"),
-        link: "https://phantom-plates.herokuapp.com/",
-    },
-    {
-        title: "J.A.T.E",
-        subtitle: "Progressive Web Applications",
-        description: "A text editor website built to provide fuctionality similar to native mobile apps.",
-        image: require("./images/JATE.gif"),
-        link: "https://serene-scrubland-88503.herokuapp.com/",
-    },
-    {
-        title: "Dan's Note Taker",
-        subtitle: "Express/Node.js",
-        description: "This was my first foray into the world of Express.js. With this program, users will be able to write and save notes with the added feature of deleting unwanted items ",
-        image: require("./images/Note Taker.gif"),
-        link: "https://dan-currier-note-taker.herokuapp.com/",
-    },
-    {
-        title: "Snake 2: 2 Snakes 2 Furious",
-        subtitle: "First Frontend Project",
-        description: "A snake game designed to generate snake facts with every high score via Wikipedia's web API.",
-        image: require("./images/snake-game.gif"),
-        link: "https://lorddanklin.github.io/snake-game/",
-    },
-    {
-        title: "Couchy",
-        subtitle: "Final Bootcamp Project",
-        description: "A travel accomodation website that provides listings from the unlikliest of places. Where's my couch?",
-        image: require("./images/couchy.png"),
-        link: "https://the-real-couchy.herokuapp.com/",
-    }
-]
+  {
+    title: "Phantom Plates",
+    subtitle: "Model View Controllers",
+    description:
+      "An ecommerce application designed to provide foodies with delicious homemade meals and homecooks with an outlet for their passion.",
+    image: require("./images/phantom-plates.png"),
+    link: "https://phantom-plates.herokuapp.com/",
+  },
+  {
+    title: "Snake 2: 2 Snakes 2 Furious",
+    subtitle: "First Frontend Project",
+    description:
+      "A snake game designed to generate snake facts with every high score via Wikipedia's web API.",
+    image: require("./images/snake-game.gif"),
+    link: "https://lorddanklin.github.io/snake-game/",
+  },
+  {
+    title: "Couchy",
+    subtitle: "Final Bootcamp Project",
+    description:
+      "A travel accomodation website that provides listings from the unlikliest of places. Where's my couch?",
+    image: require("./images/couchy.png"),
+    link: "https://the-real-couchy.herokuapp.com/",
+  },
+];
 
-export const skills = ["JavaScript", "Node.js", "MySQL", "React", "Express.js", "MongoDB", "Progressive Web Applications", "CSS", "HTML", "Public Speaking", "Writing", "Communications"];
+export const skills = [
+  "JavaScript",
+  "Node.js",
+  "MySQL",
+  "React",
+  "Express.js",
+  "MongoDB",
+  "Progressive Web Applications",
+  "CSS",
+  "HTML",
+  "Public Speaking",
+  "Writing",
+  "Communications",
+];
